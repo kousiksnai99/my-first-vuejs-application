9aeaae10496eb32ca492764283f827b5f7188229
smilegupta <guptamiley3012@gmail.com>
2020-09-18 06:15:59 +0530

    added a fake api

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import { uuid } from 'vue-uuid'; 
+// import { uuid } from 'vue-uuid'; 
 export default {
     name: "AddTodo",
     data(){
@@ -20,7 +20,6 @@ export default {
         addTodo(e){
             e.preventDefault();
             const newTodo = {
-                id: uuid.v1(),
                 title: this.title,
                 completed: false
             }
